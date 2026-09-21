76964bb6e4d2ce82f0361812cb53167be6e909eb
badpccc <estudosjoaoand@gmail.com>
2026-09-20 23:14:21 -0300

    create model/Candidato.java

---
+++ src/main/java/model/Candidato.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Candidato {
+    private Long id;
+    private String nome;
+    private int numero;
+
+    public Candidato(){
+    }
+
+    public Candidato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public Candidato(Long id, String nome, int numero) {
+        this.id = id;
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public Long getid() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String nome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+}
